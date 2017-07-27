84779a430366d3a27ab1740dace08278bf697513
Oscar Sánchez <tinocoscar11@unitec.edu>
2017-07-27 07:15:50 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -47,18 +47,18 @@ char getPrimeraLetra(string* a)
 //Definicion de palindromo: http://es.wikipedia.org/wiki/Pal%C3%ADndromo
 //Ejemplos de palindromos: Ana, arenera, arepera, anilina, ananá, Malayalam, Neuquén, Oruro, oso, radar, reconocer, rotor, salas, seres, somos, sometemos
 bool esPalindromo(string* a)
-{//Esta string almacenara lo que contenga a
+{
     string Pal = *a;
-    //esta variable sera una cadena vacia
+    
     string Inversa = "";
 
-    //el for recorrera todo el arreglo mientras i sea menor que el tamaño de la variable Pal
+    
     for (int i = 0; i < Pal.size(); i++)
     {
-        // aqui se invierte la palabra sustrayendo cada uno de los caracteres de Pal y mandandolos a Inversa a la posicion opuesta y sumandole lo que tiene inversa
+       
         Inversa = Pal.substr(i, 1) + Inversa;
     }
-    // este if solo compara lo que tenga Pal con la inversa, si da 0 significa que son iguales, si da algo diferente retorna false
+   
     if (Pal.compare(Inversa) == 0)
         return true ;
 
@@ -70,15 +70,14 @@ bool esPalindromo(string* a)
 //Desreferencia base (dado) y exponente (dado) y devuelve la base elevado al exponente
 int getExponente(int* base, int* exponente)
 {
-    //total contendra la respuesta
+   
     int total;
-    // n sera igual a lo que contenga base (dado)
+   
     int n;
     n = *base;
     for (int x =1; x<(*exponente); x++)
     {
-        //el for recorre todo el arreglo hasta que x sea menor que el exponente
-        //total sera igual a lo que tenga n por la base
+       
         total = n*= *base;
     }
     return total;
